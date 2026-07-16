52583f84f86751fc5fa929c3e0b060b5321302f6
ViscondeDev <willianoacpf@gmail.com>
2026-07-16 14:32:22 -0300

    fixed walking sound

--- scenes/chef/audio_set.gd
+++ scenes/chef/audio_set.gd
@@ -1,6 +1,6 @@
 @icon("res://addons/at-icons/node2d/note.svg")
 class_name ChefAudioSet
-extends Node
+extends Node2D
 
 @export var jump_sounds: Array[AudioStreamPlayer2D]
 @export var walk_sounds: Array[AudioStreamPlayer2D]
@@ -9,9 +9,9 @@ var is_walking: bool = false
 var current_walking_stream: AudioStreamPlayer2D
 
 
-func _physics_process(delta: float) -> void:
-	if not current_walking_stream == null:
-		if not current_walking_stream.playing and is_walking:
+func _physics_process(_delta: float) -> void:
+	if is_walking:
+		if current_walking_stream == null or not current_walking_stream.playing:
 			_play_walk()
 
 
@@ -23,6 +23,6 @@ func jump() -> void:
 
 func _play_walk() -> void:
 	randomize()
-	var sound: = jump_sounds[randi() % jump_sounds.size()]
+	var sound: = walk_sounds[randi() % walk_sounds.size()]
 	sound.play()
 	current_walking_stream = sound
